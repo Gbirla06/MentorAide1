2000a28b783d18d024b702fa242f1909e1a57d8c
Ganesh <gbirla06@gmail.com>
2024-02-24 22:47:58 +0530

    Task6 Assignment 3 completed

---
+++ Task6/Assignment3.js
@@ -0,0 +1,34 @@
+let cars = [];
+
+let car = {};
+
+
+cars.push({
+    make : "Toyota",
+    model : "Camry",
+    year : 2018
+});
+cars.push({
+    make : "Toyota",
+    model : "Camry",
+    year : 2018
+});
+cars.push({
+    make : "Toyota",
+    model : "Camry",
+    year : 2018
+});
+
+cars.shift();
+
+cars.push({
+    make : "Honda",
+    model : "Civic",
+    year : 2020
+});
+
+
+
+cars[1].model = "Accord";
+
+console.log(cars);
